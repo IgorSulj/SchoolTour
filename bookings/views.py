6bc3d63fb95565b9f30aa112000e24bd672e7b7c
IgorSulj <igorsuljin@gmail.com>
2022-08-08 18:44:12 +0300

    Created bookings app and added templates

---
+++ bookings/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import FormView
+
+# Create your views here.
+
+
+# TODO: Make this class send data to Telegram channel through bot
+class OrderView(FormView):
+    pass
+
+
--- schooltour/urls.py
+++ schooltour/urls.py
@@ -22,5 +22,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tours.urls')),
+    path('bookings/', include('booking.tours')),
     path('contacts/', TemplateView.as_view(template_name='contacts.html'), name='contacts'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
