894a89892d3eaf5903d52214659adf9b8460cd29
KiCeL <igorsuljin@gmail.com>
2022-12-11 16:26:38 +0300

    Reverting production branch merge

--- passenger_wsgi.py
+++
@@ -1,6 +0,0 @@
-import sys, os 
-cwd = os.getcwd() 
-sys.path.append(cwd) 
-os.environ['DJANGO_SETTINGS_MODULE'] = "schooltour.settings"
-from django.core.wsgi import get_wsgi_application 
-application = get_wsgi_application()
