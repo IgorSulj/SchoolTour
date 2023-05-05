e35b168d0488a84babf79004f285ef80320ab9bb
IgorSulj <78973304+IgorSulj@users.noreply.github.com>
2023-05-05 22:05:07 +0300

    HOTFIX for contacts to work

--- schooltour/urls.py
+++ schooltour/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tours.urls')),
     path('contacts/', TemplateView.as_view(template_name='contacts.html'), name='contacts'),
+    path('', include('tours.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
