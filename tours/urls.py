e6be4ef7ae3f3184067fc7b83e0c7eee4d39de11
IgorSulj <igorsuljin@gmail.com>
2023-05-06 21:31:37 +0300

    Added separate main page

--- tours/urls.py
+++ tours/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from django.views.generic import DetailView
 
 from .models import Tour
-from .views import calendar_view, category_view, TourDestinationView
+from .views import main_page, calendar_view, category_view, TourDestinationView
 
 app_name = 'tours'
 
 urlpatterns = [
-    path('', category_view, name='main_page'),
+    path('', main_page, name='main_page'),
     path('categories/<slug:slug>/', category_view, name='category'),
     path('tours/<slug:slug>', DetailView.as_view(model=Tour, template_name='tour.html'), name='tour'),
     path('calendar/', calendar_view, name='calendar'),
